01ace1b00345d54c9c5f7ca99d6ffb808511d332
mick-somprasert <mick_sir43@hotmail.com>
2020-07-23 12:39:37 +0700

    Updates

---
+++ precache-manifest.cf8612206caab3fa338b1e0ca8060954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43b0e799a2e9454a68aa6e03e5505072",
+    "url": "/basic-react2/index.html"
+  },
+  {
+    "revision": "3a07e86c5c8e9f004465",
+    "url": "/basic-react2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7d961d7d849e5fd649b3",
+    "url": "/basic-react2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a07e86c5c8e9f004465",
+    "url": "/basic-react2/static/js/2.649fd765.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/basic-react2/static/js/2.649fd765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d961d7d849e5fd649b3",
+    "url": "/basic-react2/static/js/main.a74dc367.chunk.js"
+  },
+  {
+    "revision": "aafc4444d226cdd2f839",
+    "url": "/basic-react2/static/js/runtime-main.84a7ccaa.js"
+  }
+]);
